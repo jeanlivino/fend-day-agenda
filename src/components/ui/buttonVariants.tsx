59abb98c5b0dee635c4995a7625740bec0baeaa3
AbraaoAlves <abraao.teodosio@gmail.com>
2024-07-14 00:54:45 -0300

    Check test and lint (#26)
    * chore: upgrade testing library tool
    
    * config: add test ghAction
    
    * config: add eslint ignore
    
    * fix: remove two exports
    
    * fix: remove unnecessary build step
    
    - this is don't required (render.com already do this in deploy preview)

--- src/components/ui/buttonVariants.tsx
+++ src/components/ui/buttonVariants.tsx
@@ -1,10 +1,4 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
 import { cva } from "class-variance-authority";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
 
 export const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
