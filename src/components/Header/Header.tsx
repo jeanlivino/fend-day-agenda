1badcba13a292b13a6b07b66d65923ce20b822bc
João Vitor Farias <84153149+jv-farias@users.noreply.github.com>
2024-07-16 23:37:25 -0300

    fix: switch logo in header (#45)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import imageHeader from "@/assets/Group 1.png"
+import logoFrontEnd from "@/assets/frontend-day.svg"
 import { Separator } from "@/components/ui/separator"
 import { HeaderProps } from "./types";
 
@@ -6,7 +6,7 @@ import { HeaderProps } from "./types";
 export const Header = ({ label }: HeaderProps) => {
   return (
     <div className="flex items-center justify-center flex-col gap-5">
-      <img src={imageHeader} />
+      <img src={logoFrontEnd} />
       <p className="text-2xl text-white font-bold">{label}</p>
       <Separator className="w-20 h-1 rounded-2xl bg-[#7c3aed]" />
     </div>
