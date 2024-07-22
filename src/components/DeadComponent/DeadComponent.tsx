e52ae08db0280f628bcde86450f2a09b4d847c3f
João Vitor Farias <84153149+jv-farias@users.noreply.github.com>
2024-07-21 22:00:22 -0300

    style: adjust dead component (#64)

--- src/components/DeadComponent/DeadComponent.tsx
+++ src/components/DeadComponent/DeadComponent.tsx
@@ -1,15 +1,11 @@
 import { DeadComponentProps } from "./types";
 
-export const DeadComponent = ({title, hours}: DeadComponentProps) => {
-  
-
+export const DeadComponent = ({ title, hours }: DeadComponentProps) => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="bg-[#261537] w-full sm:w-[620px] h-[62px] rounded-[8px] p-4 sm:p-12 flex items-center justify-start">
-        <div className="text-[#E6D5F7] text-xl sm:text-3xl flex items-center">
-          <span className="text-[#9888A8] mr-5">{hours}</span>
-          <p>{title}</p>
-        </div>
+    <div className="bg-[#261537] rounded-[8px] w-full max-w-[500px] p-4 flex items-center justify-start">
+      <div className="text-[#E6D5F7] text-xl flex items-center">
+        <span className="text-[#9888A8] mr-5">{hours}</span>
+        <p>{title}</p>
       </div>
     </div>
   );
