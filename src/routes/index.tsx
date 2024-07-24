012da8b02bee89664712d93f7961954dc43ad77d
Jean Livino <jeanlivino@gmail.com>
2024-07-24 20:33:46 -0300

    ci: use basename agenda

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,3 @@
-
 import { HomePage } from "@/pages/HomePage";
 import { LivePage } from "@/pages/LivePage";
 import { MyAgenda } from "@/pages/MyAgenda";
@@ -8,8 +7,8 @@ export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/agenda" element={<MyAgenda/>} />
-      <Route path="/lives" element={<LivePage />} />
+      <Route path="/minha-lista" element={<MyAgenda />} />
+      <Route path="/live" element={<LivePage />} />
       <Route path="*" element={<h1>404</h1>} />
     </Routes>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,33 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+      "@": path.resolve(__dirname, "src"),
+    },
   },
-  test:{
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "agenda/assets/[name].[hash][extname]",
+        chunkFileNames: "agenda/[name].[hash].js",
+        entryFileNames: "agenda/[name].[hash].js",
+      },
+    },
+  },
+  test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
     include: ["src/**/*.test.{ts,js,tsx,jsx}"],
     typecheck: {
-      enabled: false
-    }
-  }
-})
+      enabled: false,
+    },
+  },
+});
