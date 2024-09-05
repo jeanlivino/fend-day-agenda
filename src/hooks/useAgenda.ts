3edb6c380966c07f6b45c449817314ceb960b78f
Jean Livino <jeanlivino@gmail.com>
2024-09-05 14:33:35 -0300

    add stale time to 10 minutes

--- src/hooks/useAgenda.ts
+++ src/hooks/useAgenda.ts
@@ -1,5 +1,6 @@
 import { getAgenda } from "@/api";
 import { useQuery } from "react-query";
 
-export const useAgenda = () =>
-  useQuery(["agenda"], { queryFn: getAgenda, staleTime: Infinity });
+const tenMinutes = 1000 * 60 * 10;
+
+export const useAgenda = () => useQuery(["agenda"], { queryFn: getAgenda, staleTime: tenMinutes });
